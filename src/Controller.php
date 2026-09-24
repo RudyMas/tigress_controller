b3c56d800ad3cfbeda7f584a8c5457c319ea33aa
RudyMasGoNext <rudy.mas@go-next.be>
2026-09-24 10:27:05 +0200

    Changed to Apache License 2.0

--- src/Controller.php
+++ src/Controller.php
@@ -7,7 +7,7 @@ namespace Tigress;
  *
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2025, rudymas.be. (http://www.rudymas.be/)
- * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * @license https://opensource.org/licenses/Apache-2.0 Apache License, Version 2.0
  * @version 2025.12.09.0
  * @package Tigress\Controller
  */
